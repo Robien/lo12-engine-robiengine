1cceaef41d4bfa3539c6437dec9a7a9d4ec1069b
cephise.louison@gmail.com <cephise.louison@gmail.com@10383192-f93b-28db-3381-d362ca484740>
2012-11-29 22:23:58 +0000

    implementation de Material

--- TP LO12/TP1/Material.h
+++ TP LO12/TP1/Material.h
@@ -1,11 +1,48 @@
 #ifndef _MATERIAL_H	
 #define _MATERIAL_H
 
+#include "Vector3d.h"
+#include "Outil.h"
+
 class Material
 {
 public:
 	Material();
+	Material(Material const& c);
 	virtual ~Material();
+
+	bool isTextured();
+	GLdouble getAlpha();
+	Vector3d<GLdouble>*    getAmbiante();
+	Vector3d<GLdouble>*    getDiffuse();
+	Vector3d<GLdouble>*    getEmission();
+	Vector3d<GLdouble>*    getSpeculaire();
+	GLdouble getBrillance();      
+	GLint getIndiceTex(); 
+
+	void setTextured(bool ist);
+	void getAlpha(GLdouble al);
+	void getAmbiante(Vector3d<GLdouble>* am);
+	void getAmbiante(GLdouble r, GLdouble g, GLdouble b);
+	void getDiffuse(Vector3d<GLdouble>* di);
+	void getDiffuse(GLdouble r, GLdouble g, GLdouble b);
+	void getEmission(Vector3d<GLdouble>* em);
+	void getEmission(GLdouble r, GLdouble g, GLdouble b);
+	void getSpeculaire(Vector3d<GLdouble>* sp);
+	void getSpeculaire(GLdouble r, GLdouble g, GLdouble b);
+	void getBrillance(GLdouble br);      
+	void getIndiceTex(GLint num); 
+
+
+private:
+	bool _textured;
+	GLdouble _alpha;							/* coeff. de transparence */
+	Vector3d<GLdouble>*    _ambiante;		/* couleur de la reflectance ambiante */
+	Vector3d<GLdouble>*    _diffuse;			/* couleur de la reflectance diffuse */
+	Vector3d<GLdouble>*    _emission;		/* couleur de la reflectance emise */
+	Vector3d<GLdouble>*    _speculaire;		/* couleur de la reflectance speculaire */
+	GLdouble _brillance;						/* coeff. de brillance */
+	GLint _indiceTex;						/* indice de la texture */
 };
 
 #endif
