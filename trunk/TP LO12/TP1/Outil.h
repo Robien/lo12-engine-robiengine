ed9a01e368124320fcd1bce0ed3367a95b0a68a6
r.guyard@gmail.com <r.guyard@gmail.com@10383192-f93b-28db-3381-d362ca484740>
2012-11-29 17:57:10 +0000

    add méthodes dans Face

--- trunk/TP LO12/TP1/Outil.h
+++ trunk/TP LO12/TP1/Outil.h
@@ -5,6 +5,7 @@
 #include "Scene.h"
 #include "Pngloader.h"
 #include "Singleton.h"
+#include "Vector3d.h"
 
 
 class Outil : public Singleton<Outil>
@@ -12,6 +13,19 @@ class Outil : public Singleton<Outil>
 public:
 	void initManager(Scene* scene);
 	GLuint addTexture(char* filename);
+
+public:
+	template <typename T>
+	Vector3d<T>* produitVectoriel(Vector3d<T> u, Vector3d<T> v)
+	{
+		Vector3d<T>* w = new Vector3d<T>();
+
+		w->setX(u.getY() * v.getZ() - u.getZ() * v.getY());
+		w->setY(u.getZ() * v.getX() - v.getZ() * u.getX());
+		w->setZ(u.getX() * v.getY() - v.getX() * u.getY());
+
+		return w;
+	}
 
 	private:
 	Scene* _scene;
