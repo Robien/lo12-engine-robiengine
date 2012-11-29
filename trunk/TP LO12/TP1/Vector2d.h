c40e343d8648ec2a615e7d62fb5b28d042df641e
r.guyard@gmail.com <r.guyard@gmail.com@10383192-f93b-28db-3381-d362ca484740>
2012-11-29 15:43:33 +0000

    add Vector2d

---
+++ trunk/TP LO12/TP1/Vector2d.h
@@ -0,0 +1,63 @@
+#ifndef _VECTOR2D_H	
+#define _VECTOR2D_H
+
+template <typename T>
+class Vector2d
+{
+public:
+	Vector2d(){}
+	Vector2d(const Vector2d<T>& c)
+	{
+		_x = c.getX();
+		_y = c.getY();
+	}
+	Vector2d(T x, T y) :_x(x), _y(y)
+	{
+
+	}
+	virtual ~Vector2d(){}
+
+public:
+	const T getX()
+	{
+		return _x;
+	}
+	const T getY()
+	{
+		return _y;
+	}
+	void setX(T x)
+	{
+		_x = x;
+	}
+	void setY(T y)
+	{
+		_y = y;
+	}
+	Vector2d<T> operator+(const Vector2d<T>& other)
+	{
+		return Vector2d<T>(other._x + _x, other._y + _y);
+	}
+	Vector2d<T> operator-(const Vector2d<T>& other)
+	{
+		return Vector2d<T>(other._x - _x, other._y - _y);
+	}
+	
+
+public:
+	virtual void print()
+	{
+		std::cout << "X=" << _x << ", Y=" << _y;
+	}
+	virtual void println()
+	{
+		std::cout << "X=" << _x << ", Y=" << _y << std::endl;
+	}
+
+private:
+	T _x, _y;
+
+};
+
+
+#endif
