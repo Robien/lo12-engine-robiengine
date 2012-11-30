1ebb292c95b660cff61503d872803020a1094fdf
r.guyard@gmail.com <r.guyard@gmail.com@10383192-f93b-28db-3381-d362ca484740>
2012-11-30 00:03:33 +0000

    1ere implémentation des objets (et abstract)

--- trunk/TP LO12/TP1/AbstractObjet.cpp
+++ trunk/TP LO12/TP1/AbstractObjet.cpp
@@ -9,3 +9,19 @@ AbstractObjet::AbstractObjet()
 AbstractObjet::~AbstractObjet()
 {
 }
+
+
+void AbstractObjet::addFils(AbstractObjet* fils)
+{
+	_fils.push_back(fils);
+}
+
+void AbstractObjet::removeFils(AbstractObjet* fils)
+{
+	_fils.remove(fils);
+}
+
+std::list<AbstractObjet*> AbstractObjet::getFils()
+{
+	return _fils;
+}
--- trunk/TP LO12/TP1/AbstractObjet.h
+++ trunk/TP LO12/TP1/AbstractObjet.h
@@ -1,6 +1,8 @@
 #ifndef _ABSTRACT_OBJ_H	
 #define _ABSTRACT_OBJ_H
 
+#include <list>
+
 class AbstractObjet
 {
 public:
@@ -8,6 +10,24 @@ public:
 	virtual ~AbstractObjet();
 
 	virtual void affiche() = 0;
+
+public:
+	void addFils(AbstractObjet* fils);
+	inline void attache(AbstractObjet* fils)
+	{
+		addFils(fils);
+	}
+	void removeFils(AbstractObjet* fils);
+	inline void dettache(AbstractObjet* fils)
+	{
+		removeFils(fils);
+	}
+
+protected:
+	std::list<AbstractObjet*> getFils();
+
+private:
+	std::list<AbstractObjet*> _fils;
 
 };
 
