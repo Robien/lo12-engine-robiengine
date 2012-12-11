c253462d95752b499d149c306163fbca09746480
r.guyard@gmail.com <r.guyard@gmail.com@10383192-f93b-28db-3381-d362ca484740>
2012-12-11 21:40:11 +0000

    ajout du culling ! commit 100, bogoss inside !

--- TP LO12/TP1/Collider.h
+++ TP LO12/TP1/Collider.h
@@ -26,12 +26,18 @@ public:
 public:
 	void affiche();
 	void afficheWithMat(Matrice<GLdouble>* mat, Matrice<GLdouble>* m);
+	void majMatrice(Matrice<GLdouble>* mat);
+
+	GLdouble getDistanceMax();
 
 private:
 	Vector3d<GLdouble> _CoordMin;
 	Vector3d<GLdouble> _CoordMax;
 
 	bool _isSet;
+
+	GLdouble _distanceMax;
+	Matrice<GLdouble> _matrice;
 
 };
 
