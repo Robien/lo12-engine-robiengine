7aedcda6963ab804083b93454da5bced8578d81f
cephise.louison@gmail.com <cephise.louison@gmail.com@10383192-f93b-28db-3381-d362ca484740>
2012-12-17 13:36:20 +0000

    Ajout de fonction pour avoir la taille de la fenetre dans Outil

--- trunk/TP LO12/TP1/Outil.cpp
+++ trunk/TP LO12/TP1/Outil.cpp
@@ -1,1 +1,16 @@
 #include "Outil.h"
+
+int Outil::getHauteurFenetre()
+{
+	GLint m_viewport[4];
+	glGetIntegerv( GL_VIEWPORT, m_viewport );
+	return m_viewport[3];
+}
+
+int Outil::getLargeurFenetre()
+{
+	GLint m_viewport[4];
+
+	glGetIntegerv( GL_VIEWPORT, m_viewport );
+	return m_viewport[2];
+}
