98dd1cb2dd566a03176ff874dbe47031fd6e1afc
r.guyard@gmail.com <r.guyard@gmail.com@10383192-f93b-28db-3381-d362ca484740>
2013-01-10 12:51:23 +0000

    add gyrophare

--- TP LO12/TP1/AnimationPorte.cpp
+++ TP LO12/TP1/AnimationPorte.cpp
@@ -1,7 +1,7 @@
 #include "AnimationPorte.h"
 
 
-AnimationPorte::AnimationPorte(AbstractObjet* objlie) : Animation(objlie)
+AnimationPorte::AnimationPorte(AbstractObjet* objlie, AnimationRotation* gyro) : Animation(objlie), _gyro(gyro) 
 {
 	_isOpening = false;
 	_isClosing = false;
@@ -19,46 +19,48 @@ void AnimationPorte::update()
 {
 	if(_actif)
 	{
-	if (_isOpening)
-	{
-		_isOpen = false;
-		_isClose = false;
-		_angle += 0.5;
-		getActeur()->getMatrice()->translate(
-			getActeur()->getAxes().at(0)->getA()->getX(),
-			getActeur()->getAxes().at(0)->getA()->getY(),
-			getActeur()->getAxes().at(0)->getA()->getZ());
-		getActeur()->getMatrice()->rotate(0.5,0,0,1);
-				getActeur()->getMatrice()->translate(
-			-getActeur()->getAxes().at(0)->getA()->getX(),
-			-getActeur()->getAxes().at(0)->getA()->getY(),
-			-getActeur()->getAxes().at(0)->getA()->getZ());
-		if (_angle >= 85)
+		if (_isOpening)
 		{
-			_isOpening = false;
-			_isOpen = true;
-		}
-	} 
-	else if (_isClosing)
-	{
-		_isOpen = false;
-		_isClose = false;
-		_angle -= 0.2;
-				getActeur()->getMatrice()->translate(
-			getActeur()->getAxes().at(0)->getA()->getX(),
-			getActeur()->getAxes().at(0)->getA()->getY(),
-			getActeur()->getAxes().at(0)->getA()->getZ());
-		getActeur()->getMatrice()->rotate(-0.2,0,0,1);
-				getActeur()->getMatrice()->translate(
-			-getActeur()->getAxes().at(0)->getA()->getX(),
-			-getActeur()->getAxes().at(0)->getA()->getY(),
-			-getActeur()->getAxes().at(0)->getA()->getZ());
-		if (_angle <= 0)
+			_isOpen = false;
+			_isClose = false;
+			_angle += 0.5;
+			getActeur()->getMatrice()->translate(
+				getActeur()->getAxes().at(0)->getA()->getX(),
+				getActeur()->getAxes().at(0)->getA()->getY(),
+				getActeur()->getAxes().at(0)->getA()->getZ());
+			getActeur()->getMatrice()->rotate(0.5,0,0,1);
+					getActeur()->getMatrice()->translate(
+				-getActeur()->getAxes().at(0)->getA()->getX(),
+				-getActeur()->getAxes().at(0)->getA()->getY(),
+				-getActeur()->getAxes().at(0)->getA()->getZ());
+			if (_angle >= 85)
+			{
+				_isOpening = false;
+				_isOpen = true;
+				_gyro->setActif(false);
+			}
+		} 
+		else if (_isClosing)
 		{
-			_isClosing = false;
-			_isClose = true;
+			_isOpen = false;
+			_isClose = false;
+			_angle -= 0.2;
+					getActeur()->getMatrice()->translate(
+				getActeur()->getAxes().at(0)->getA()->getX(),
+				getActeur()->getAxes().at(0)->getA()->getY(),
+				getActeur()->getAxes().at(0)->getA()->getZ());
+			getActeur()->getMatrice()->rotate(-0.2,0,0,1);
+					getActeur()->getMatrice()->translate(
+				-getActeur()->getAxes().at(0)->getA()->getX(),
+				-getActeur()->getAxes().at(0)->getA()->getY(),
+				-getActeur()->getAxes().at(0)->getA()->getZ());
+			if (_angle <= 0)
+			{
+				_isClosing = false;
+				_isClose = true;
+				_gyro->setActif(false);
+			}
 		}
-	}
 	}
 }
 
@@ -66,11 +68,13 @@ void AnimationPorte::proxEvent(GLdouble distance)
 {
 	if (distance < 80000 && !_isOpen)
 	{
+		_gyro->setActif(true);
 		_isOpening = true;
 		_isClosing = false;
 	}
 	else if(distance >= 80000 && !_isClose)
 	{
+		_gyro->setActif(true);
 		_isOpening = false;
 		_isClosing = true;
 	}
